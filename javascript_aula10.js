4e53c22401e63b1a14305c741ba1ad2c79047df5
RinaldoSilva1968 <rinaldo.sg@gmail.com>
2023-01-17 20:25:56 -0300

    Array

---
+++ javascript_aula10.js
@@ -0,0 +1,23 @@
+//Array
+//Formal
+let myarr=new Array('Rinaldo','webdesign',10);
+console.log(myarr);
+
+//Literal
+let myArr2=['Laura','estudante',{school:'center'}];
+console.log(myArr2);
+
+//Associativo
+
+let myArrAss={
+    name: 'Rinaldo',
+    age:54
+}
+myArr2[0]='other';
+myArr2[3]=14587;
+console.log(myArrAss);
+console.log(myArr2[2]);
+console.log(myArr2[2].school);
+console.log(myArr2.length);
+console.log(myarr.constructor);
+console.log(myArr2.prototype);
