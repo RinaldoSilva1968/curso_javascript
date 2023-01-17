c4066382f5c78e3a8b74988a252c5ba5db931697
RinaldoSilva1968 <rinaldo.sg@gmail.com>
2023-01-17 20:34:57 -0300

    Tema variado

---
+++ aula.js
@@ -0,0 +1,57 @@
+// // let idade = 5;
+// // console.log(idade);
+// // let altura=165;
+// // console.log(altura);
+
+//  //variáveis
+// let nome = "Rinaldo";
+// console.log(nome);
+// let idade = 53;
+// console.log(idade);
+// let altura = 165;
+// console.log(altura);
+// //para introduzir a aula de cosntantes
+// let valorIngressoAdulto=20;
+// valorIngressoAdulto=30;
+// console.log(valorIngressoAdulto);
+
+// // constantes
+// const valorIngressoInfantil=10;
+// // você nunca pode redefinir um valor de uma cosntante
+// //valorIngressoAdulto=30;
+// console.log(valorIngressoInfantil);
+
+// // tipos primitivos
+//  let lugar = "Russas"; //string literal
+// console.log(lugar);
+// let num = 25.35; // number literal
+// console.log(num);
+// let estaAprovdo=true; // boolean
+// console.log(estaAprovdo);
+// let sobrenome=undefined; // undefined pouco utilizada
+//  let corSelecionado = null; // qdo se quer redefinir um valor
+
+// // tipagem dinâmica
+
+// // objetos
+//  let pessoa = {
+//     nome:"Rinaldo",
+//     idade: 53,
+//     estaAprovdo: true,
+//     sobrenome: "da Silva Gonçalves"
+//  };
+//  console.log(pessoa);
+
+ //arrays
+//é um conjunto de dados que poderm ser acessados por um índice
+// let familia = [26,45,50,17];
+// console.log(familia); 
+//let meuNome = ['Rinaldo',53,1968,'Russas'];
+//console.log(meuNome.length);
+//console.log(meuNome[2,3]);
+
+//função
+// verbo + substantivo
+ 
+
+
