a539674960491bbbc279b95958ca67658796c691
RinaldoSilva1968 <rinaldo.sg@gmail.com>
2023-01-16 21:37:20 -0300

    Observações JavaScript

---
+++ javascript_aula06.js
@@ -0,0 +1,27 @@
+// O javascript é keysensitive
+//Ignora espaços nos códigos 
+//em stringe ele observa os espaços 
+// Em expressões regulares os espaços também são observados
+/*
+let name='Rinaldo';
+let Name='Laura';
+console.log(Name);
+*/
+//console.log(window);
+//window.alert('Oi');
+/*
+alert('oi');
+console.log(window.outerWidth);
+console.log(outerWidth);
+*/
+/*
+this.user='Webdesign';
+console.log(window);
+*/
+/*
+this.user='Webdesign';
+function TextDecoderStream(){
+    console.log(this);
+}
+TextDecoderStream();
+*/
