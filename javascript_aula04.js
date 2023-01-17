a9f504f212a071d4591670032fe559782f105eb7
RinaldoSilva1968 <rinaldo.sg@gmail.com>
2023-01-16 21:25:46 -0300

    aula 4: condicionais

---
+++ javascript_aula04.js
@@ -0,0 +1,31 @@
+let age =25;
+/*
+if(age > 18){
+    console.log('Ele é maior de idade');
+}else if(age == 18){
+    console.log('Ele tem exatamente a idade');
+}else{
+    console.log('Ele é menor de idade');
+}
+*/
+//Operador ternário
+
+//(age>=18)?console.log('Ele é maior de idade'):console.log('Ele é menor de idade');
+
+/*
+let year=1;
+
+switch(year){
+    case 1:
+    console.log('Janeiro');
+    break;
+    case 2:
+    console.log('Fevereiro');
+    break;
+    case 3:
+    console.log('Março');
+    break;
+    default:
+    console.log('Mês inválido');
+    break;
+*/
